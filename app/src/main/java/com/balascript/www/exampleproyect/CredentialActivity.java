c09c01031e0e4ec2e0ec40f37af5533c50afbeb2
luiguibalarezo@gmail.com <@.Ll7957621>
2017-02-27 00:35:05 -0500

    1

--- app/src/main/java/com/balascript/www/exampleproyect/CredentialActivity.java
+++ app/src/main/java/com/balascript/www/exampleproyect/CredentialActivity.java
@@ -4,11 +4,10 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.balascript.www.exampleproyect.baseclass.BaseAppCompatActivity;
 import com.balascript.www.exampleproyect.fragment.ForgotPasswordFragment;
@@ -32,7 +31,7 @@ public class CredentialActivity extends BaseAppCompatActivity {
 
           /*Clases, Metodos Asociados a Goggle*/
         if (savedInstanceState == null) {
-            transactionFragment(sigup_fragment, null, "add", false);
+            transactionFragment(signin_fragment, null, "add", false);
         }
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
--- app/src/main/java/com/balascript/www/exampleproyect/fragment/SignInFragment.java
+++ app/src/main/java/com/balascript/www/exampleproyect/fragment/SignInFragment.java
@@ -1,18 +1,42 @@
 package com.balascript.www.exampleproyect.fragment;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.balascript.www.exampleproyect.R;
 import com.balascript.www.exampleproyect.baseclass.BaseFragment;
+
+import butterknife.BindView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class SignInFragment extends BaseFragment {
+
+
+    @BindView(R.id.img_logo)
+    ImageView img_logo;
+    @BindView(R.id.edt_user)
+    EditText edt_user;
+    @BindView(R.id.edt_pass)
+    EditText edt_pass;
+    @BindView(R.id.btn_enter)
+    Button btn_enter;
+    @BindView(R.id.btn_forgot_password)
+    Button btn_forgot_password;
+    @BindView(R.id.btn_signup)
+    Button btn_signup;
+    @BindView(R.id.btn_facebook)
+    Button btn_facebook;
+    @BindView(R.id.btn_twitter)
+    Button btn_twitter;
+    @BindView(R.id.btn_google)
+    Button btn_google;
 
     public SignInFragment() {
 
